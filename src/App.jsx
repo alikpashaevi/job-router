fe97489a379d85dd5e1c9530be943a857143b419
alikpashaevi <alikpashaev06@gmail.com>
2024-08-01 00:53:23 +0400

    form react router

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Careers, { careersLoader } from './pages/careers/Careers'
 import RootLayout from './layouts/RootLayout'
 import HelpLayout from './layouts/HelpLayout'
 import Faq from './pages/help/Faq'
-import Contact from './pages/help/Contact'
+import Contact, { contactAction } from './pages/help/Contact'
 import NotFound from './pages/NotFound'
 import CareerLayout from './layouts/CareerLayout'
 import CareerDetails, {careerDetailsLoader} from './pages/careers/CareerDetails'
@@ -25,7 +25,7 @@ const router = createBrowserRouter(
 
       <Route path='help' element={<HelpLayout />}>
         <Route path='faq' element={<Faq />}/>
-        <Route path='contact' element={<Contact />}/>
+        <Route path='contact' element={<Contact />} action={contactAction} />
       </Route>
 
       <Route path='careers' element={<CareerLayout />} errorElement={<CareersError />}>
